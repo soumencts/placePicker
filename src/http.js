cc32979e7a5579954715e99e6672f378ca1e1896
Sarkar <675123@cognizant.com>
2024-03-28 16:21:02 +0530

    project init

---
+++ src/http.js
@@ -0,0 +1,32 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const responseData = await response.json();
+    if(!response.ok){
+      throw new Error('Failed to fetch places');
+    }
+    return responseData.places;  
+}
+
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const responseData = await response.json();
+    if(!response.ok){
+      throw new Error('Failed to fetch user places');
+    }
+    return responseData.places;  
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places: places}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const responseData = await response.json();
+    if(!response.ok){
+      throw new Error('Failed to update user data.');
+    }
+    return responseData.message;  
+}
